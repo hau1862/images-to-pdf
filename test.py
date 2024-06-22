a2882686984d45aede410109e5bded00156c35cf
Nguyen Tien Hau <hau1862@gmail.com>
2024-06-22 17:08:47 +0700

    Init Project

---
+++ test.py
@@ -0,0 +1,15 @@
+from math import sqrt
+
+factor = sqrt(2)
+min_diff = 1
+width_result = 0
+
+for width in range(1920, 3000):
+	temp = width*factor
+	diff = abs(round(temp) - temp)
+	
+	if diff < min_diff:
+		min_diff = diff
+		width_result = width
+
+print(f"({width_result}, {round(width_result*factor)})", min_diff)
